aa50bb423d78413d5129d65372986f3b96ccc02d
Felix Burr <felix@DESKTOP-V8KO5C9.localdomain>
2022-03-10 23:44:03 +0100

    saw 8

--- ComicNEAT/c_src/comic.c
+++ ComicNEAT/c_src/comic.c
@@ -688,7 +688,7 @@ void collect_gold()
 //   score = increased by 2000 points
 void collect_item()
 {
-  fitness += 42; // item gives 2000 points
+  fitness += 256; // item gives 2000 points
   PLAY_SOUND(SOUND_COLLECT_ITEM);
   award_points(20);
   // Mark item as collected in items_collected.
@@ -2463,7 +2463,7 @@ EXPORTED double tick(uint8_t jump_key_pressed, uint8_t open_key_pressed, uint8_t
       handle_teleport(); // skip blitting the map and Comic, because handle_teleport does it
       goto handle_nonplayer_actors;
     }
-
+  uint8_t jump_initiated = 0;
   // are we jumping or falling?
   if(comic_is_falling_or_jumping != 0)
     died = died || handle_fall_or_jump(jump_key_pressed, left_key_pressed, right_key_pressed); // handle_fall_or_jump jumps back into game_loop.check_pause_input
@@ -2478,7 +2478,7 @@ EXPORTED double tick(uint8_t jump_key_pressed, uint8_t open_key_pressed, uint8_t
 	    { // Initiate a new jump
 	      comic_is_falling_or_jumping = 1;
 	      died = died || handle_fall_or_jump(jump_key_pressed, left_key_pressed, right_key_pressed);
-	      goto check_pause_input;
+	      jump_initiated = 1;
 	    }
 	}
       else
@@ -2504,6 +2504,8 @@ EXPORTED double tick(uint8_t jump_key_pressed, uint8_t open_key_pressed, uint8_t
 		}
 	    }
 	}
+      if(jump_initiated)
+	goto check_pause_input;
       // check teleport input
       if(teleport_key_pressed != 0 && comic_has_teleport_wand)
 	{ // The teleport key is being pressed. Do we have the Teleport Wand?
