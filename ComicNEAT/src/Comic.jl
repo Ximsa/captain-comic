da3e01f05c3a5937b306c087773c62f30071e54e
Felix Burr <felix@DESKTOP-V8KO5C9.localdomain>
2022-03-11 15:18:49 +0100

    Picard

--- ComicNEAT/src/Comic.jl
+++ ComicNEAT/src/Comic.jl
@@ -118,7 +118,7 @@ function center_environment(environment::Vector{Float32})
     for y = 1:size(view,2)
         for x = 1:size(view,1)
             if(view[x,y] == 3)
-                view[x,y] = 9
+                view[x,y] = 0
                 comic_x = x
                 comic_y = y
             end
